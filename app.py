99ca36b1f87f5da3382e02de14901bdabaa702ce
Wenxuan Shen <bushigemen114@gmail.com>
2026-02-16 21:32:49 +0800

    feat: 记录日志文件

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from core.main import RPMain
+from core.config.dirs import ROOT
 import sys
 from PySide6.QtWidgets import QApplication
+from loguru import logger
+
+logger.add(str(ROOT / "logs/{time:YYYY-MM-DD}.log"), rotation="00:00")
 
 
 if __name__ == "__main__":
